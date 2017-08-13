7587f004f5ddd695c83edccbeda6cb2ccbe15244
Maxwell Obi <obi.maxwell@rocketmail.com>
2017-08-13 16:31:21 +0100

    Kill and restart ngrok process on initilization

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace TCPServerApp
 {
@@ -6,13 +7,27 @@ namespace TCPServerApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to TCP Remote Server");
 
-            ServerListener server = new ServerListener();
-            server.Start().Wait();
+            if (args.Length < 2) Exit();
+            else if (!args[0].Equals("--authtoken") || string.IsNullOrEmpty(args[1])) Exit();
 
+            string ngrok_auth_token = args[1].Trim();
+
+            //kill any ngrok process already running in the background
+            foreach (var process in Process.GetProcessesByName("ngrok"))
+                process.Kill();
 
             Console.Read();
+        }
+
+        static void Exit()
+        {
+            Console.WriteLine("Your provided invalid arguments.");
+            Console.WriteLine("Usage: specify your ngrok authtoken using --authtoken ");
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
+            Process.GetCurrentProcess().Kill();
         }
     }
 }
