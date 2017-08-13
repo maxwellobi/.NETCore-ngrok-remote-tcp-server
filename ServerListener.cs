c6d3787e466c903fdb7040028f46cfdf00959ad6
Maxwell Obi <obi.maxwell@rocketmail.com>
2017-08-13 16:34:03 +0100

    Expose TCP port and start server on initization

--- ServerListener.cs
+++ ServerListener.cs
@@ -1,35 +1,29 @@
 ﻿using System;
-using System.Text;
-using System.Net.Sockets;
-using System.Net.Http;
 using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace TCPServerApp
 {
     class ServerListener
     {
-        TcpListener listener;
+        TcpListener listener = null;
 
+        public string TCPPort { get; set; }
+        
         public ServerListener() {
 
-            
-            
-        }
-
-        //start listening on local
-        //connect to remote server
-        //send ip and port to remote server
-
-        public async Task Start()
-        {
             listener = new TcpListener(IPAddress.Any, 0); //listen on all interface using a random port
             listener.Start();
 
             var endPoint = (IPEndPoint)listener.LocalEndpoint;
-            Console.WriteLine("TCP server started and listening on {0}:{1}", endPoint.Address.ToString(), endPoint.Port.ToString());
+            TCPPort = endPoint.Port.ToString();
 
-            
+            Console.WriteLine("TCP server started and listening on {1}", TCPPort);
+        }
+
+        public async Task StartListening()
+        {  
             while (true)
             {
                 Console.WriteLine("Waiting for client...");
