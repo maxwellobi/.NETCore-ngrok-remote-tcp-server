6fc64b369600fea999ba892121ab18bce8b1576e
Maxwell Obi <obi.maxwell@rocketmail.com>
2017-08-13 11:13:23 +0100

    Added Ngrok class for the tunneling operations

---
+++ Ngrok.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace TCPServerApp
+{
+    class Ngrok
+    {
+        string ngrok_download_url;
+
+        public Ngrok()
+        {
+            //If ngrok is not in root folder, download and unzip
+            if (!File.Exists("ngrok") || !File.Exists("ngrok.exe"))
+            {
+                if (string.IsNullOrEmpty(NgrokURL()))
+                {
+                    Console.WriteLine("@@ Invalid System Architecture Detected - Cannot Run Program");
+                    Console.WriteLine("@@ Press any key to exit: ");
+                    Console.Read();
+                    System.Diagnostics.Process.GetCurrentProcess().Kill();
+                }
+
+                else DownloadNgrok().Wait();
+            }
+        }
+
+        private async Task DownloadNgrok()
+        {
+            Console.WriteLine("Downloading Ngrok now ... ");
+
+           
+        }
+
+        private string NgrokURL()
+        {
+            return ngrok_download_url;
+        }
+    }
+}
